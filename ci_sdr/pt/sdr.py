3362dc37c8f5c5a9f1b229a8afb410d92e205e8e
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2021-01-15 12:00:05 +0100

    py_mir_eval_sdr: add soft_max_SDR argument, expose filter_length, remove padertorch depedency

--- ci_sdr/pt/sdr.py
+++ ci_sdr/pt/sdr.py
@@ -1,9 +1,33 @@
+import itertools
 import torch
+from ci_sdr.pt.wiener_filter import wiener_filter_predict_single_input
 
 
-def linear_to_db(numerator, denominator):
+def soft_max_SDR_to_eps(soft_max_SDR):
+    if soft_max_SDR is None:
+        return None
+    else:
+        assert soft_max_SDR > 5, soft_max_SDR
+        return 10 ** (-soft_max_SDR / 10)
+
+
+def linear_to_db(numerator, denominator, eps=None):
+    """
+    >>> linear_to_db(torch.tensor(100), torch.tensor(1))
+    tensor(20.)
+    >>> linear_to_db(torch.tensor(100), torch.tensor(1), soft_max_SDR_to_eps(10))
+    tensor(9.5861)
+    >>> linear_to_db(torch.tensor(100), torch.tensor(1), soft_max_SDR_to_eps(20))
+    tensor(16.9897)
+    >>> linear_to_db(torch.tensor(100), torch.tensor(1), soft_max_SDR_to_eps(100))
+    tensor(20.)
+
+    """
     assert denominator > 0, denominator
-    return 10 * torch.log10(numerator / denominator)
+    if eps is None:
+        return 10 * torch.log10(numerator / denominator)
+    else:
+        return -10 * torch.log10(denominator / numerator + eps)
 
 
 def py_mir_eval_sdr(
@@ -11,6 +35,8 @@ def py_mir_eval_sdr(
         estimation,  # K x T
         compute_permutation=False,
         change_sign=False,
+        filter_length=512,
+        soft_max_SDR=None,
 ):
     """
 
@@ -23,7 +49,7 @@ def py_mir_eval_sdr(
             permutation based on the SDR.
         change_sign:
             When True, assume this function is used as loss and return `-SDR`
-            instead of `SDR`. This can save intermediate tensors.
+            instead of `SDR`.
 
     Returns:
         SDR values for each source
@@ -53,6 +79,13 @@ def py_mir_eval_sdr(
     >>> py_mir_eval_sdr(torch.as_tensor(reference), torch.as_tensor(estimation[[0, 1]]), compute_permutation=True, change_sign=True)
     tensor([-11.2143, -12.1095], dtype=torch.float64)
 
+    >>> py_mir_eval_sdr(torch.as_tensor(reference), torch.as_tensor(estimation), soft_max_SDR=20)
+    tensor([10.6748, 11.4555], dtype=torch.float64)
+    >>> py_mir_eval_sdr(torch.as_tensor(reference), torch.as_tensor(reference), soft_max_SDR=20)
+    tensor([20., 20.], dtype=torch.float64)
+    >>> py_mir_eval_sdr(torch.as_tensor(reference), torch.as_tensor(reference), soft_max_SDR=None)
+    tensor([295.1350, 269.7644], dtype=torch.float64)
+
     >>> estimation = ex['audio_data']['observation'][:2]
     >>> pb_bss.evaluation.mir_eval_sources(reference, estimation)[0]
     array([-0.26370129, -0.57722483])
@@ -68,26 +101,40 @@ def py_mir_eval_sdr(
              -6.0106e-05,  2.1169e-04]], dtype=torch.float64)
 
     """
-    from padercontrib.pytorch.ops.mir_eval_sdr.wiener_filter import pt_wiener_filter_predict_single_input
+    K, num_samples = reference.shape
 
     if compute_permutation:
-        from padertorch.ops.losses import pit_loss
+        # ToDo: Add option to use hungarian algorithm
+        #       Note:
+        #        - The hungarian algorithm cannot release intermediate tensors
+        #        - The hungarien algorithm has advantages for K > 3
 
-        def loss_fn(estimation, reference):
-            return py_mir_eval_sdr(reference, estimation, change_sign=True)
-
+        axis = 0
+        candidates = []
+        indexer = [slice(None), ] * estimation.ndim
+        permutations = list(itertools.permutations(range(K)))
+        # Does torch drop a tensor after `torch.minimum(a, b)`, when both are
+        # scalars? (i.e. release the memory).
+        #  - If yes, optimize the following loop
+        #  - If no, min(a, b) would be a option, but it would introduce a
+        #    cuda sync point, hence it is a tradeoff
+        for permutation in permutations:
+            indexer[axis] = permutation
+            candidates.append(py_mir_eval_sdr(
+                reference,
+                estimation[tuple(indexer)],
+                change_sign=False,
+                filter_length=filter_length, soft_max_SDR=soft_max_SDR
+            ))
+        sdr, idx = torch.max(torch.stack(candidates), dim=0)
         if change_sign:
-            return pit_loss(estimation, reference, loss_fn=loss_fn, axis=0)
+            return -sdr
         else:
-            return -pit_loss(estimation, reference, loss_fn=loss_fn, axis=0)
-
-    K = len(reference)
-
-    filter_length = 512
+            return sdr
 
     scores = []
     for k in range(K):
-        reverberated = pt_wiener_filter_predict_single_input(
+        reverberated = wiener_filter_predict_single_input(
             reference[k], estimation[k], filter_length=filter_length)
 
         est = estimation[k]
@@ -97,7 +144,7 @@ def py_mir_eval_sdr(
         num = torch.sum(reverberated**2)
         den = torch.sum((reverberated - est)**2)
 
-        scores.append(linear_to_db(num, den))
+        scores.append(linear_to_db(num, den, eps=soft_max_SDR_to_eps(soft_max_SDR)))
     if change_sign:
         return -torch.stack(scores)
     else:
