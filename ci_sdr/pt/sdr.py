26ea78251ce80b43a753cc54a02da6e8b7fafbd5
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2025-05-15 13:55:55 +0200

    relax doctest to work across multiple pytorch versions

--- ci_sdr/pt/sdr.py
+++ ci_sdr/pt/sdr.py
@@ -111,10 +111,14 @@ def ci_sdr_loss_hungarian(
     Example:
         >>> reference = torch.tensor([[1., 2, 1, 2], [4, 3, 2, 1], [1, 2, 3, 4]])
         >>> estimation = torch.tensor([[1., 2, 3, 4], [1, 2, 1, 2], [4, 3, 2, 1]])
-        >>> ci_sdr_loss_hungarian(estimation, reference + 0.00001, filter_length=2)  # Perfect reconstruction is not stable, add a small value
-        tensor([-114.2742, -119.2708, -116.7869])
-        >>> ci_sdr_loss(estimation, reference + 0.00001, filter_length=2)  # Perfect reconstruction is not stable, add a small value
-        tensor([-114.2742, -119.1221, -116.7869])
+
+        # Perfect reconstruction is not stable in pytorch, so add a small value to refernce
+
+        >>> ci_sdr_loss_hungarian(estimation, reference + 0.00001, filter_length=2)  # doctest: +ELLIPSIS
+        tensor([-114.2..., -119.2..., -116.7...])
+        >>> ci_sdr_loss(estimation, reference + 0.00001, filter_length=2)  # doctest: +ELLIPSIS
+        tensor([-114.2..., -119.1..., -116.7...])
+        
         >>> estimation = torch.tensor([[1., 2, 2, 4], [1, 1, 1, 2], [4, 2, 2, 1]])
         >>> ci_sdr_loss_hungarian(estimation, reference, filter_length=2)
         tensor([-10.3300, -17.2712, -15.4051])
